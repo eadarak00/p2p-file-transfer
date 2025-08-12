6d8668371457e613bc41c896c2b3c7c53debe756
eadarak00 <eadarak2001@gmail.com>
2025-08-12 16:40:19 +0000

    feat: deplacement du fichier jsonUtils

---
+++ transfert-fichiers/src/main/java/com/p2p/utils/JsonUtils.java
@@ -0,0 +1,23 @@
+package com.p2p.utils;
+
+import com.google.gson.Gson;
+import com.p2p.Metadata;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+
+public class JsonUtils {
+    private static final Gson gson = new Gson();
+
+    public static void ecrireMetadata(Metadata metadata, String chemin) throws Exception {
+        try (FileWriter writer = new FileWriter(chemin)) {
+            gson.toJson(metadata, writer);
+        }
+    }
+
+    public static Metadata lireMetadata(String chemin) throws Exception {
+        try (FileReader reader = new FileReader(chemin)) {
+            return gson.fromJson(reader, Metadata.class);
+        }
+    }
+}
