2a5a497f2f10886268332b4453193ab5dc4b30d4
eadarak00 <eadarak2001@gmail.com>
2025-08-12 16:35:46 +0000

    feat:TestCommunication.java (client demande la liste) java

--- transfert-fichiers/src/main/java/com/p2p/TestCommunication.java
+++ transfert-fichiers/src/main/java/com/p2p/TestCommunication.java
@@ -1,18 +1,30 @@
 package com.p2p;
 
-public class TestCommunication {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
 
+public class TestCommunication {
     public static void main(String[] args) throws InterruptedException {
         int port = 5000;
-        NetworkManager networkManager = new NetworkManager(port);
+        String dossier = "/chemin/vers/dossierPartage";  // adapte ici
 
-        // Démarrer le serveur TCP
-        networkManager.demarrerServeur();
+        NetworkManager serverManager = new NetworkManager(port, dossier);
+        serverManager.demarrerServeur();
 
-        // Pause pour que le serveur soit prêt
-        Thread.sleep(1000);
+        Thread.sleep(1000); // attendre que le serveur soit prêt
 
-        // Envoyer un message client vers serveur
-        networkManager.envoyerMessage("localhost", "Hello Peer");
+        NetworkManager clientManager = new NetworkManager(port, null);
+        String jsonListe = clientManager.envoyerCommande("localhost", "LIST");
+
+        Gson gson = new Gson();
+        Type listeType = new TypeToken<List<Metadata>>(){}.getType();
+        List<Metadata> liste = gson.fromJson(jsonListe, listeType);
+
+        System.out.println("Fichiers disponibles :");
+        for (Metadata m : liste) {
+            System.out.println(m.getNom() + " | taille: " + m.getTaille() + " | checksum: " + m.getChecksum());
+        }
     }
 }
